5a9b397f779146ef2224626510e08a7f3f2db228
erick-commits <erickfernandomoraramirez@gmail.com>
2020-09-27 13:44:08 -0400

    User Manager elimina los tokens excedentes y los vencidos, endpoint para login, endpoint para refrescar vigencia de token.

--- data/createUsersDatabase.sql
+++ data/createUsersDatabase.sql
@@ -24,3 +24,13 @@ CREATE TABLE UserCreationRequests(
     requestStatus INTEGER,
     creationDate INTEGER DEFAULT (strftime('%s', 'now'))
 );
+DROP VIEW IF EXISTS VW_Tokens;
+CREATE VIEW VW_Tokens AS
+SELECT Tokens.*,
+    Users.maxTokenCount,
+    row_number() over(
+        partition by Tokens.idUsuario
+        order by Tokens.expiration desc
+    ) as ordenToken
+FROM Users
+    JOIN Tokens ON Users.id = Tokens.idUsuario;
